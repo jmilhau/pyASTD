e3886773fdf1bdbfd0280e3fcea9738d1a73bba8
Jérémy Milhau <jeremilhau@gmail.com>
2012-06-14 16:18:35 +0200

    Merging with guard

--- src/astd/__init__.py
+++ src/astd/__init__.py
@@ -3,9 +3,6 @@ from .types.automaton import *
 from .types.kleene import *
 from .types.sequence import *
 from .types.choice import *
-<<<<<<< HEAD
 from .types.synchronization import *
-=======
 from .types.guard import *
->>>>>>> guard
 from .types.elem import *
